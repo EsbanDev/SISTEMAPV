b9ffa71e805c291757037281cdd0ea2ce34f9bc0
BuildTools <unconfigured@null.spigotmc.org>
2026-06-18 16:39:36 -0500

    cors actualizada, agregar frontend

--- src/main/java/com/sistemapv/config/WebConfig.java
+++ src/main/java/com/sistemapv/config/WebConfig.java
@@ -1,22 +1,21 @@
 package com.sistemapv.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebConfig {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // aplica a todos los endpoints
-                        .allowedOrigins("http://localhost:4321") // frontend de Astro
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // métodos permitidos
-                        .allowedHeaders("*") // permite todos los headers
-                        .allowCredentials(true); // si usas cookies o Authorization
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://sistemapv-frotend-simple.vercel.app"  // Frontend en Vercel
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
+
